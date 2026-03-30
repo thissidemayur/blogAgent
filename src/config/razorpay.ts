0291ec016c454590ccef22a7debd3a1dbdb846b4
thissidemayur <thissidemayur@gmail.com>
2026-03-30 18:47:23 +0530

    fix: make razorpay_key_id as public

--- src/config/razorpay.ts
+++ src/config/razorpay.ts
@@ -2,7 +2,7 @@ import Razorpay from "razorpay";
 import { env } from "./env";
 
 export const razorpay = new Razorpay({
-  key_id: env.RAZORPAY_KEY_ID,
+  key_id: env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
   key_secret: env.RAZORPAY_KEY_SECRET,
 });
 
