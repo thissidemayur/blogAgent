d34c9d1e31044054229b4aec0f1d8f86889feacc
thissidemayur <thissidemayur@gmail.com>
2026-03-30 17:20:51 +0530

    feat: add razorpay,cleark,prisma env variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,13 @@ import {z} from "zod"
 const envSchema = z.object({
   TAVILY_API_KEY: z.string(),
   OPENAI_API_KEY: z.string(),
+  CLERK_WEBHOOK_SECRET: z.string(),
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  CLERK_SECRET_KEY: z.string(),
+  DATABASE_URL: z.string(),
+  RAZORPAY_KEY_ID: z.string(),
+  RAZORPAY_KEY_SECRET:z.string(),
+  RAZORPAY_WEBHOOK_SECRET:z.string()
 });
 
 const _env = envSchema.safeParse(process.env)
