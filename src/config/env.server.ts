3417088444750879836b0ffd9dd63ac17a83bbb3
thissidemayur <thissidemayur@gmail.com>
2026-04-01 22:43:47 +0530

    feat: change file name from env.ts to env.server.ts

--- src/config/env.server.ts
+++ src/config/env.server.ts
@@ -4,10 +4,8 @@ const envSchema = z.object({
   TAVILY_API_KEY: z.string(),
   OPENAI_API_KEY: z.string(),
   CLERK_WEBHOOK_SECRET: z.string(),
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
   DATABASE_URL: z.string(),
-  NEXT_PUBLIC_RAZORPAY_KEY_ID: z.string(),
   RAZORPAY_KEY_SECRET: z.string(),
   RAZORPAY_WEBHOOK_SECRET: z.string(),
   RAZORPAY_CALLBACK_URL:z.string()
@@ -16,7 +14,7 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 if(!_env.success) {
     console.error(`[ENV_ERROR]: missing env variable`)
-    console.error(_env.error.message)
+    console.error(_env.error.issues)
     process.exit(1)
 }
 
