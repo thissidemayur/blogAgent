23758a2c0872c34d963c7ff34b69aea7bc91abde
thissidemayur <thissidemayur@gmail.com>
2026-04-01 22:46:18 +0530

    feat: change env import from 'env.ts'  to 'env.server.ts'

--- src/config/razorpay.ts
+++ src/config/razorpay.ts
@@ -1,8 +1,9 @@
 import Razorpay from "razorpay";
-import { env } from "./env";
+import { env } from "./env.server";
+import { clientEnv } from "./env.client";
 
 export const razorpay = new Razorpay({
-  key_id: env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+  key_id: clientEnv.NEXT_PUBLIC_RAZORPAY_KEY_ID,
   key_secret: env.RAZORPAY_KEY_SECRET,
 });
 
